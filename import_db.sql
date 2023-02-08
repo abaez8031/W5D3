8728dddd442bb3ba8ed670337f0afbf8056fddca
abaez8031 <abaez8031@gmail.com>
2023-02-08 11:53:31 -0500

    finished tables

--- import_db.sql
+++ import_db.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON
+
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -20,19 +22,34 @@ CREATE TABLE questions (
 DROP TABLE IF EXISTS question_follows;
 
 CREATE TABLE question_follows (
+  id INTEGER PRIMARY KEY
+  user_id INTEGER NOT NULL
+  question_id INTEGER NOT NULL
 
+  FOREIGN KEY (user_id) REFERENCES users(id)
+  FOREIGN KEY (question_id) REFERENCES questions(id)
 )
 
 DROP TABLE IF EXISTS replies;
 
 CREATE TABLE replies (
   id INTEGER PRIMARY KEY
-  reply_id INTEGER NOT NULL
-  user_reply INTEGER NOT NULL
+  user INTEGER NOT NULL
   question_id INTEGER NOT NULL
+  reply TEXT NOT NULL
 
 
-  FOREIGN KEY (reply_id) REFERENCES questions(id)
-  FOREIGN KEY (user_reply) REFERENCES users(id)
+  FOREIGN KEY (user) REFERENCES users(id)
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+  FOREIGN KEY (id) REFERENCES replies(id)
 )
 
+CREATE TABLE question_likes (
+
+  id INTEGER PRIMARY KEY
+  question INTEGER NOT NULL
+  author_id INTEGER NOT NULL
+
+  FOREIGN KEY (question) REFERENCES questions(id)
+  FOREIGN KEY (author_id) REFERENCES users(id)
+)
