97f33803daa4b7af97f9de789c61a95f3c0cbc7f
abaez8031 <abaez8031@gmail.com>
2023-02-08 16:28:10 -0500

    finish db

--- import_db.sql
+++ import_db.sql
@@ -1,74 +1,126 @@
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS question_likes;
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS users;
+
 PRAGMA foreign_keys = ON;
 
-DROP TABLE IF EXISTS students;
 
-CREATE TABLE students (
+CREATE TABLE users (
   id INTEGER PRIMARY KEY,
-  fname TEXT NOT NULL,
-  lname TEXT NOT NULL
+  fname VARCHAR(255) NOT NULL,
+  lname VARCHAR(255) NOT NULL
 );
 
-DROP TABLE IF EXISTS questions;
+INSERT INTO
+  users (fname, lname)
+VALUES
+  ("Ariel", "Baez"),
+  ("Keval", "Shah");
+
 
 CREATE TABLE questions (
   id INTEGER PRIMARY KEY,
-  title TEXT NOT NULL,
+  title VARCHAR(255) NOT NULL,
   body TEXT NOT NULL,
-  associated_author INTEGER NOT NULL,
+  author_id INTEGER NOT NULL,
 
-  FOREIGN KEY (associated_author) REFERENCES students(id)
+  FOREIGN KEY (author_id) REFERENCES users(id)
 );
 
-DROP TABLE IF EXISTS question_follows;
+INSERT INTO
+  questions (title, body, author_id)
+SELECT
+  "Ariel Question", "ARIEL ARIEL ARIEL", 1
+FROM
+  users
+WHERE
+  users.fname = "Ariel" AND users.lname = "Baez";
+
+INSERT INTO
+  questions (title, body, author_id)
+SELECT
+  "Keval Question", "KEVAL KEVAL KEVAL", users.id
+FROM
+  users
+WHERE
+  users.fname = "Keval" AND users.lname = "Shah";
 
 CREATE TABLE question_follows (
   id INTEGER PRIMARY KEY,
   user_id INTEGER NOT NULL,
   question_id INTEGER NOT NULL,
 
-  FOREIGN KEY (user_id) REFERENCES students(id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (question_id) REFERENCES questions(id)
 );
 
-DROP TABLE IF EXISTS replies;
+INSERT INTO
+  question_follows (user_id, question_id)
+VALUES
+  ((SELECT id FROM users WHERE fname = "Ariel" AND lname = "Baez"),
+  (SELECT id FROM questions WHERE title = "Keval Question")),
+
+  ((SELECT id FROM users WHERE fname = "Keval" AND lname = "Shah"),
+  (SELECT id FROM questions WHERE title = "Keval Question")
+);
+
+
+-- REPLIES
+
 
 CREATE TABLE replies (
   id INTEGER PRIMARY KEY,
-  user INTEGER NOT NULL,
   question_id INTEGER NOT NULL,
-  reply TEXT NOT NULL,
-
-
-  FOREIGN KEY (user) REFERENCES students(id),
+  parent_reply_id INTEGER,
+  author_id INTEGER NOT NULL,
+  body TEXT NOT NULL,
   FOREIGN KEY (question_id) REFERENCES questions(id),
-  FOREIGN KEY (id) REFERENCES replies(id)
+  FOREIGN KEY (parent_reply_id) REFERENCES replies(id),
+  FOREIGN KEY (author_id) REFERENCES users(id)
 );
 
-DROP TABLE IF EXISTS question_likes;
+INSERT INTO
+  replies (question_id, parent_reply_id, author_id, body)
+VALUES
+  ((SELECT id FROM questions WHERE title = "Keval Question"),
+  NULL,
+  (SELECT id FROM users WHERE fname = "Ariel" AND lname = "Baez"),
+  "How do you do?"
+);
+
+INSERT INTO
+  replies (question_id, parent_reply_id, author_id, body)
+VALUES
+  ((SELECT id FROM questions WHERE title = "Keval Question"),
+  (SELECT id FROM replies WHERE body = "How do you do?"),
+  (SELECT id FROM users WHERE fname = "Ariel" AND lname = "Baez"),
+  "I am doing just fine."
+);
+
 
 CREATE TABLE question_likes (
-
   id INTEGER PRIMARY KEY,
-  question INTEGER NOT NULL,
-  author_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
 
-  FOREIGN KEY (question) REFERENCES questions(id),
-  FOREIGN KEY (author_id) REFERENCES students(id)
+  FOREIGN KEY(user_id) REFERENCES users(id),
+  FOREIGN KEY(question_id) REFERENCES questions(id)
 );
 
 INSERT INTO
-  students(fname,lname)
+  question_likes (user_id, question_id)
 VALUES
-  ('Ariel', 'Baez'),
-  ('Keval', 'Shah');
+  ((SELECT id FROM users WHERE fname = "Ariel" AND lname = "Baez"),
+  (SELECT id FROM questions WHERE title = "Keval Question")
+);
 
-INSERT INTO
-  questions(title,body,associated_author)
-VALUES
-  ('Ariel Question', 'ARIEL ARIEL ARIEL', (SELECT id FROM students WHERE fname = 'Ariel')),
-  ('Keval Question', 'KEVAL KEVAL KEVAL', (SELECT id FROM students WHERE fname = 'Keval'));
-
--- INSERT INTO
---   question_follows(user_id,question_id)
--- VALUES
---   ((SELECT id FROM students WHERE fname = 'Ariel'), (SELECT id FROM questions WHERE associated_author = students.id));
+INSERT INTO 
+  question_likes (user_id, question_id) 
+VALUES 
+  (1, 1);
+INSERT INTO 
+  question_likes (user_id, question_id) 
+VALUES 
+  (1, 2);
